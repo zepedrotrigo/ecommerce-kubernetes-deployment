c28cfb9ddcf39d7d8113829ed875d8793e18742a
Pedro Monteiro <pmapm@ua.pt>
2023-04-13 13:52:29 +0100

    add frontend folder

---
+++ frontend/src/components/Banner/Banner.js
@@ -0,0 +1,44 @@
+import styles from './styles.module.css'
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay } from "swiper";
+import "swiper/css";
+import Typed from "react-typed";
+
+const Banner = () => {
+    SwiperCore.use([Autoplay]);
+
+    return (
+        <>
+            <p className={styles.logoHeader}>
+                <Typed
+                    strings={[
+                        "Sneak in style with Sneakr.",
+                    ]}
+                    typeSpeed={35}
+                />
+            </p>
+            <div className={styles.container}>
+                <Swiper
+                    spaceBetween={0}
+                    slidesPerView={1}
+                    loop={true}
+                    autoplay={{
+                        delay: 2000
+                    }}
+                >
+                    <SwiperSlide>
+                        <img src="sneaker1.jpg" alt="slide1" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="sneaker2.jpg" alt="slide2" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="sneaker5.jpg" alt="slide3" />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </>
+    )
+}
+
+export default Banner;
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import Navbar from "../../components/Navbar/Navbar";
+import Banner from "../../components/Banner/Banner";
+import ProductBanner from "../../components/ProductBanner/ProductBanner";
+
+
+const Home = ()=>{
+    return (
+        <div>
+            <Navbar />
+            <Banner />
+        </div>
+    )
+}
+
+export default Home;
