358b0b62aa4cb084a36ab1548a9c55facb70cd75
Pedro Monteiro <pmapm@ua.pt>
2023-04-15 10:55:35 +0100

    create login page, missing input font awesome icons

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -2,58 +2,66 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { login } from "../../actions/authActions";
-import Navbar from "../../components/Navbar/Navbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import './Login.css';
 
-
-const Login = ()=>{
-    const { isAuthenticated } = useSelector((state)=>state.authReducer)
+const Login = () => {
+    const { isAuthenticated } = useSelector((state) => state.authReducer)
     const dispatch = useDispatch();
-    const [formData,setFormData] = useState({
-        email:'',
-        password:''
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
     })
 
-    const {email,password} = formData;
+    const { email, password } = formData;
 
-    const onChange = e=>setFormData({...formData,[e.target.name]:e.target.value});
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onClick = ()=>{
-        dispatch(login(email,password))
+    const onClick = () => {
+        dispatch(login(email, password))
     }
 
-    useEffect(()=>{
-        if(isAuthenticated){
-           window.location.replace('/member')
+    useEffect(() => {
+        if (isAuthenticated) {
+            window.location.replace('/member')
         }
-    },[dispatch,isAuthenticated])
-    
+    }, [dispatch, isAuthenticated])
+
     return (
-        <div>
-            <Navbar />
-            <div>
-                <input type="text" 
-                name="email" 
-                placeholder="Email" 
-                value={email} 
-                onChange={e=>onChange(e)} 
-                required />
-            </div>
-            <div>
-                <input type="password" 
-                name="password" 
-                placeholder="Password" 
-                value={password} 
-                onChange={e=>onChange(e)} 
-                required />
-            </div>
-            <div>
-                <button onClick={onClick}>Login</button>
-            </div>
-            <div>
-                <h3>Not have account ?</h3>
-                <Link to={'/signup'}>
-                    <span>Register here</span>
-                </Link>
+        <div className="login-page">
+            <div className="left-image"></div>
+            <div className="right-inputs">
+                <div className="login-form">
+                    <h3>Login</h3>
+                        <input 
+                        className="email-input"
+                        type="text"
+                            name="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={e => onChange(e)}
+                            />
+                        <input type="password"
+                        className="password-input"
+                            name="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={e => onChange(e)}
+                            />
+                    <div className="forgot-password">
+                        <span>Forgot password?</span>
+                    </div>
+                    <div style={{width: '100%'}}>
+                        <button className="login-button" onClick={onClick}>Login</button>
+                    </div>
+                    <div className="no-account">
+                        <p className="no-account-header">Not have account ?</p>
+                        <Link to={'/signup'} style={{textDecoration: 'none'}}>
+                            <span className="signup-redirect">Create your Account<FontAwesomeIcon icon={faArrowRight} style={{marginLeft: '0.3rem'}} /></span>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
